19ea307e1874105806936fcd23d305011ec4ff0a
Sam Jackson <smahstan@gmail.com>
2017-09-17 09:35:15 -0500

    Can hide keyboard now

---
+++ ui.js
@@ -0,0 +1,13 @@
+var keyboardHidden = false
+
+$("#hide-keyboard").click( () => {
+	if (keyboardHidden == false) {
+		keyboardHidden = true
+		$('#keyboard').hide()
+		$("#hide-keyboard").text('^')
+	} else if (keyboardHidden == true) {
+		keyboardHidden = false
+		$('#keyboard').show()
+		$("#hide-keyboard").text('v')
+	}
+})
