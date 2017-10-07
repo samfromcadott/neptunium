071620b2922273f3a60a58b967b6e8d4643c332d
Sam Jackson <smahstan@gmail.com>
2017-10-07 11:14:45 -0500

    Basic zoom and storing node positions

--- ui.js
+++ ui.js
@@ -6,16 +6,17 @@ var nodeTree = {
 function addNode() {
 	if ( Object.keys(nodeTree).length == 1 ) { //If no nodes exist except inputNote
 		nodeTree[0] = {} //Add node zero
+		return nodeTree[0]
 	} else {
 		var nodeList = Object.keys(nodeTree) //Create Array of keys in nodeTree
 		var lastKey = parseInt( nodeList[nodeList.length - 2] ) //Get highest numbered node (The very last node will always be inputNote)
 
 		nodeTree[lastKey + 1] = {} //Create new node with key one higher than current highest
+		return nodeTree[lastKey + 1]
 	}
 }
 
 function updateUiHandlers() { //Adds jquery events to new nodes
-	$('.node').draggable()
 	$('#background-grid').draggable()
 }
 
@@ -24,21 +25,54 @@ $('.tool-item').mousedown( () => {
 		//Get mouse location in current div
 		var mousePos = {x: event.offsetX, y: event.offsetY}
 
+		var newNode = addNode()
+
 		//Create and add node
-		$('<div />', {
+		var newNodeDiv = $('<div />', {
 			class: 'node'
-		}).css({
+		})
+
+		newNodeDiv.css({
 			top: mousePos.y + 'px',
 			left: mousePos.x + 'px'
-		}).appendTo('#background-grid')
+		})
 
-		//Add node to nodeTree
-		addNode()
+		newNodeDiv.appendTo('#background-grid')
+
+		newNode.position = { //Store node's position
+			top: mousePos.y,
+			left: mousePos.x
+		}
+
+		newNodeDiv.draggable({
+			drag: function() {
+				newNode.position = $(this).position() //Update node position
+
+			}
+
+		})
+
 		console.log(nodeTree)
 
-		updateUiHandlers()
+		//Add node to nodeTree
 		$(this).unbind(event)
 	})
+})
+
+$('#node-view').bind( 'mousewheel', (event) => {
+	// console.log(event.originalEvent.deltaY)
+	var currentZoom = $('#node-view').css('zoom')
+	console.log(currentZoom)
+
+	if (event.originalEvent.deltaY > 0) {
+		currentZoom *= 0.75
+	} else if (event.originalEvent.deltaY < 0) {
+		currentZoom *= 1.5
+	}
+
+	// var zoomAmount = 2 * ( event.originalEvent.deltaY / Math.abs(event.originalEvent.deltaY) ) + 1
+	// console.log('Zoom amount: ', zoomAmount)
+	$('#node-view').animate({ 'zoom': currentZoom}, 50)
 })
 
 // Keyboard
